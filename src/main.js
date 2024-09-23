d886f162044fd0324b2b0982751db875895187a4
arhamfareed106 <arhamfareed575@gmail.com>
2024-09-23 16:30:30 +0500

    production

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    root: '.', // Ensure this points to where your index.html is located
+    build: {
+      outDir: 'dist', // Where the production build will be output
+    },
+  };
+  
